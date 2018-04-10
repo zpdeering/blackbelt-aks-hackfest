7dffe4baf14d7425b507ac8cda9e2df6828b05ab
Zachary Deering <zdeerin1@jhmi.edu>
2018-04-10 09:50:14 -0400

    More updates

--- app/web/src/components/Footer.vue
+++ app/web/src/components/Footer.vue
@@ -14,7 +14,7 @@
       <div class="col-lg-6">
       </div>
       <div class="col-lg-12 credits">
-        Azure Global Blackbelt Team
+        Johns Hopkins IT@JH Technology Innovation Center
       </div>
       <div class="col-lg-6">
       </div>
